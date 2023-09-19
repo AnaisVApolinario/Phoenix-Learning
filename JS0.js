7a281b3052bbf63b1fa5ed9431d932e28919851f
Anais <avalverdea54@gmail.com>
2023-09-19 17:39:31 -0500

    Resolviendo el ejercicio de variables

---
+++ JS0.js
@@ -0,0 +1,40 @@
+//Crea variables siguiendo las reglas de nomenclatura adecuadas.
+
+//Crea 10 variables de tipo string (cadena de texto) y asignale cualquier texto. 
+let saludo = "Hola, mundo!";
+let nombreMascota = "Askar";
+let fruta = "Piña";
+let marca = "Adidas";
+let apellido = "Villa";
+let instrumento = "Piano";
+let color= "celeste";
+let comida = "arroz chaufa";
+let pais="Perú";
+let cantante = "Rafa <3";
+
+
+//Crea 10 variables de tipo number (numero) y asígnale cualquier número.
+let maximoFrutas = 10;
+let edad = 19;
+let niñas = 5;
+let nota1 = 18;
+let nota2= 15;
+let nota3 = 16;
+let promedio = 16.33;
+let faltas=5;
+let sueldo =1800;
+let mascotas= 2;
+
+
+
+//Crea 10 variables de tipo boolean (true/false) y asígnale cualquier valor.
+let puedeCorrer = true;
+let estaActivo=true;
+let puedeEscribir= false;
+let tieneFrutas=true;
+let estaDisponible=false;
+let puedeLeer=true;
+let tieneNumeros=false;
+let estaModoOscuro=true;
+let puedeAvanzar=false;
+let tienePermisos=false;
